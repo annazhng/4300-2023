443247c0f4d43043bc60016c4eefc9846e4499f8
Dave Jung <sj597@cornell.edu>
2023-03-26 00:00:20 -0400

    Change in JSON to SQL function

--- backend/helpers/MySQLDatabaseHandler.py
+++ backend/helpers/MySQLDatabaseHandler.py
@@ -3,9 +3,10 @@ import sqlalchemy as db
 from sqlalchemy import text
 import json
 
+
 class MySQLDatabaseHandler(object):
-    
-    def __init__(self,MYSQL_USER,MYSQL_USER_PASSWORD,MYSQL_PORT,MYSQL_DATABASE,MYSQL_HOST = "localhost"):
+
+    def __init__(self, MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE, MYSQL_HOST="localhost"):
         self.IS_DOCKER = True if 'DB_NAME' in os.environ else False
         self.MYSQL_HOST = os.environ['DB_NAME'] if self.IS_DOCKER else MYSQL_HOST
         self.MYSQL_USER = "admin" if self.IS_DOCKER else MYSQL_USER
@@ -16,7 +17,8 @@ class MySQLDatabaseHandler(object):
 
     def validate_connection(self):
 
-        engine = db.create_engine(f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}")
+        engine = db.create_engine(
+            f"mysql+pymysql://{self.MYSQL_USER}:{self.MYSQL_USER_PASSWORD}@{self.MYSQL_HOST}:{self.MYSQL_PORT}")
         conn = engine.connect()
         conn.execute(f"CREATE DATABASE IF NOT EXISTS {self.MYSQL_DATABASE}")
         conn.execute(f"USE {self.MYSQL_DATABASE}")
@@ -24,8 +26,8 @@ class MySQLDatabaseHandler(object):
 
     def lease_connection(self):
         return self.engine.connect()
-    
-    # new helper written to write JSON files to SQL table 
+
+    # new helper written to write JSON files to SQL table
     def insert_data_from_json(self, json_file_path, table_name):
         conn = self.lease_connection()
         with open(json_file_path, 'r') as f:
@@ -33,32 +35,34 @@ class MySQLDatabaseHandler(object):
         for item in data:
             item = {k: v if v != '' else None for k, v in item.items()}
             insert_query = text(f"INSERT INTO {table_name} (name, hotel_class, region, street_address, postal_code, \
-                                locality, id, service, cleanliness, value, review_text) VALUES (:hotel_class, \
+                                locality, id, service, cleanliness, value, review_text) VALUES (:name, :hotel_class, \
                                 :region, :streetaddress, :postalcode, :locality, :id, :service, :cleanliness, \
                                 :value, :text)")
             conn.execute(insert_query, **item)
-    
-    def query_executor(self,query):
+
+    def query_executor(self, query):
         conn = self.lease_connection()
         if type(query) == list:
             for i in query:
                 conn.execute(i)
         else:
             conn.execute(query)
-        
-    def query_selector(self,query):
+
+    def query_selector(self, query):
         conn = self.lease_connection()
         data = conn.execute(query)
         return data
 
-    def load_file_into_db(self,file_path  = None):
+    def load_file_into_db(self, file_path=None):
         if self.IS_DOCKER:
             return
         if file_path is None:
-            file_path = os.path.join(os.environ['ROOT_PATH'],'init.sql')
-        sql_file = open(file_path,"r")
-        sql_file_data = list(filter(lambda x:x != '',sql_file.read().split(";\n")))
+            file_path = os.path.join(os.environ['ROOT_PATH'], 'init.sql')
+        sql_file = open(file_path, "r")
+        sql_file_data = list(
+            filter(lambda x: x != '', sql_file.read().split(";\n")))
+
         self.insert_data_from_json('relevant_fields.json', 'hotel_reviews')
         self.query_executor(sql_file_data)
+        print('gang2')
         sql_file.close()
-
