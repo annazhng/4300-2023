d78a5e21e8207e35e0268f662f9a5a78ea256b2c
Dave Jung <sj597@cornell.edu>
2023-04-13 18:29:37 -0400

    New Model

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,15 @@
 import json
 import os
+import numpy as np
+from collections import defaultdict
+from nltk.tokenize import word_tokenize
 from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 
-# ROOT_PATH for linking with all your files.
+# ROOT_PATH for linking with all your files. 
 # Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
+os.environ["ROOT_PATH"] = os.path.abspath(os.path.join("..", os.curdir))
 
 # These are the DB credentials for your OWN MySQL
 # Don't worry about the deployment credentials, those are fixed
@@ -17,18 +20,23 @@ MYSQL_PORT = 3306
 MYSQL_DATABASE = "hotels"
 
 mysql_engine = MySQLDatabaseHandler(
-    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE)
+    MYSQL_USER, MYSQL_USER_PASSWORD, MYSQL_PORT, MYSQL_DATABASE
+)
 
 # Path to init.sql file. This file can be replaced with your own file for testing on localhost, but do NOT move the init.sql file
 mysql_engine.load_file_into_db()
-
 app = Flask(__name__)
 CORS(app)
-
 # Sample search, the LIKE operator in this case is hard-coded,
 # but if you decide to use SQLAlchemy ORM framework,
 # there's a much better and cleaner way to do this
 
+def jaccard_sim(text1, text2):
+    text1_set = set(word_tokenize(text1))
+    text2_set = set(word_tokenize(text2))
+    intersection = len(text1_set.intersection(text2_set))
+    union = len(text1_set.union(text2_set))
+    return len(intersection) / len(union)
 
 def sql_search(user_input):
     query_sql = f"""
@@ -39,26 +47,24 @@ def sql_search(user_input):
         AVG(cleanliness) >= '%s' AND
         AVG(service) >= '%s' AND
         AVG(value) >= '%s'
-    ORDER BY (sum(cleanliness) + sum(service) + sum(value)) DESC
-    LIMIT 10;
-    """%(user_input['cleanliness'], user_input['service'], user_input['value'])
-
-    '''
-    query_sql = f"""
-    SELECT name, avg(service),avg(cleanliness), avg(value), locality, review_text
-    FROM hotel_reviews
-    GROUP BY name
-    HAVING
-        MIN(cleanliness) >= '%%{user_input['cleanliness']}%%' AND
-        MIN(service) >= '%%{user_input['service']}%%' AND
-        MIN(value) >= '%%{user_input['value']}%%'
-    ORDER BY (sum(cleanliness) + sum(service) + sum(value)) DESC
-    LIMIT 10;
-    """'''
-
-    keys = ["name", "service", "cleanliness", "value", "locality","review_text"]
+    WHERE locality = '%s'
+    ORDER BY (sum(cleanliness) + sum(service) + sum(value)) ASC
+    LIMIT 100;
+    """ % (
+        user_input["cleanliness"],
+        user_input["service"],
+        user_input["value"],
+        user_input['locality']
+    )
+    keys = ["name", "service", "cleanliness", "value", "locality", "review_text"]
     data = mysql_engine.query_selector(query_sql)
-    return [dict(zip(keys, i)) for i in data]
+    dataset = [dict(zip(keys, i)) for i in data]
+    jacc_scores = []
+    for rev in dataset: 
+        score = jaccard_sim(user_input["text"], rev['review_text'])
+        jacc_scores.append(score)
+    arg_sort = np.argsort(jacc_scores)
+    return [dataset[i] for i in arg_sort]
 
 
 @app.route("/", methods=['GET'])
@@ -73,18 +79,10 @@ def home():
         output = sql_search(user_input)
     return render_template('base.html', service=service, cleanliness=cleanliness, value=value, valid_form=valid_form, output=output)
 
-
-@app.route("/hotel_reviews")
-def hotels_search():
-    text = request.args.get("name")
+@app.route("/episodes")
+def episodes_search():
+    text = request.args.get("title")
     return sql_search(text)
 
+
 # app.run(debug=True)
-
-# enter mysql shell
-# /usr/local/mysql/bin/mysql -uroot -p
-
-# show contents of table
-# USE hotels;
-# SHOW TABLES;
-# SELECT * FROM hotel_reviews;
