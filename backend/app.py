30cd28d7b28fd3c6b39b942a3b2b9722d9a825c1
Dave Jung <sj597@cornell.edu>
2023-04-13 21:03:22 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -68,7 +68,7 @@ def sql_search(user_input):
         score = jaccard_sim(user_input["text"], rev['review_text'])
         jacc_scores.append(score)
     arg_sort = np.argsort(jacc_scores)
-    return [dataset[i] for i in arg_sort]
+    return [dataset[i] for i in arg_sort].reverse()
 
 
 @app.route("/", methods=['GET'])
